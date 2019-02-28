935202e412dcd6464c0b4373a5056586c0975923
shivam goel <shivamgoel132@gmail.com>
2019-03-01 00:07:41 +0530

    Prime Number till N

---
+++ prime.py
@@ -0,0 +1,25 @@
+# Author : Shivam Goel
+
+def isPrime(n):
+    # corner case
+    if n <= 1:
+        return False
+
+    # Check from 2 to n
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+def CheckPrime(n):
+    for i in range (2, n + 1):
+        if isPrime(i):
+            print(i, end = " ")
+
+
+if __name__ == '__main__':
+    n = input("Enter the value of N")
+    q = int (n)
+    printPrime (q)
